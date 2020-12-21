b6edceb6d10cbf6f94a9b59ac45d5456a1bfa536
Fabrizio <fabrizio.carlassara@gmail.com>
2020-12-21 03:06:26 -0300

    Initial commit

---
+++ diodo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+	const double is = 50E-9;    // La corriente de saturacion del diodo va a ser de 50nA    
+    const int v = 5;            // La tension de alimentacion del circuito es de 5V
+
+    double vd = 0.4;    // La tension del diodo para comenzar a iterar, elegimos 0.4V porque 
+                        // sabemos que la caida de tension del diodo deberia andar por ahi
+
+    /*   Aca les toca a ustedes   */
+    
+}
