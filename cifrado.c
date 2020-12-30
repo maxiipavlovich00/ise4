92f74e7a1cdafa91df47109c689870cbcd914cba
Fabrizio <fabrizio.carlassara@gmail.com>
2020-12-30 02:36:41 -0300

    Initial commit

---
+++ cifrado.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int validate(int argc, char *argv[]);
+char* cifrar(char *text, char *key);
+
+int main(int argc, char *argv[]) {
+
+    if (validate(argc, argv)) {
+        return 1;
+    }
+
+    // TODO
+
+    return 0;
+}
+
+int validate(int argc, char *argv[]) {
+    
+    // TODO
+}
+
+char* cifrar(char *text, char *key) {
+
+    // TODO
+}
