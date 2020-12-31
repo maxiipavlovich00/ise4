e46ff8a40cf59901090201f01a1c80ce99fe039f
Fabrizio <fabrizio.carlassara@gmail.com>
2020-12-31 01:37:33 -0300

    Initial commit

---
+++ word.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 1024
+
+int read_file(char *text, char *filename);
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Uso: ./word frases/file.txt\n");
+        return 1;
+    }
+
+    char text[MAX];
+
+    if (read_file(text, argv[1])) {
+        printf("Archivo no encontrado\n");
+        return 1;
+    }
+
+    // TODO
+
+    return 0;
+}
+
+int read_file(char *text, char *filename) {
+
+    FILE *fp;
+    char c;
+
+    fp = fopen(filename, "r");
+    
+    if (!fp) {
+        return 1;
+    } 
+    while ((c = getc(fp)) != EOF) {
+        *text = c;
+        text++;
+    }
+    return 0;
+}
