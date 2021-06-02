f782bf7a7ee6338ec2a299ab1bfe3a14ac6bc5bf
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-06-02 03:06:33 -0300

    Update cifrado.c

--- cifrado.c
+++ cifrado.c
@@ -16,37 +16,10 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/*
-*   Funcion: validar
-*   ---------------------------------------
-*   Verifica que la clave de cifrado 
-*   ingresada:
-*   - Tenga 26 caracteres
-*   - No repita letras
-*   - Solo contenga letras
-*   
-*   @param: argc - cantidad de argumentos
-*   @param: argv - argumentos
-*
-*   @return: cero si es valida, uno si no
-*/
-
 int validar(int argc, char *argv[]) {
     
     // TODO
 }
-
-/*  
- *  Funcion: cifrar
- *  ----------------------------------------
- *  Cifra un texto a partir de una clave
- *  determinada
- *
- *  @param: text - texto a cifrar
- *  @param: key - clave valida para cifrar
- *
- *  @return: texto cifrado
- */
 
 char* cifrar(char *text, char *key) {
 
